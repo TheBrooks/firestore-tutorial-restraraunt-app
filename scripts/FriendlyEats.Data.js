c10a155827acb73274ee00554175df8e635eda53
TheBrooks <the.ryan.brooks@gmail.com>
2018-02-06 13:25:23 -0700

    added a get restaraunt by id and a filtered search

--- scripts/FriendlyEats.Data.js
+++ scripts/FriendlyEats.Data.js
@@ -40,15 +40,30 @@ FriendlyEats.prototype.getDocumentsInQuery = function(query, render) {
 };
 
 FriendlyEats.prototype.getRestaurant = function(id) {
-  /*
-    TODO: Retrieve a single restaurant
-  */
+  return firebase.firestore().collection('restaurants').doc(id).get();
 };
 
 FriendlyEats.prototype.getFilteredRestaurants = function(filters, render) {
-  /*
-    TODO: Retrieve filtered list of restaurants
-  */
+  let query = firebase.firestore().collection('restaurants');
+
+  if(filters.category !== 'Any'){
+    query = query.where('category', '==', filters.category);
+  }
+  if(filters.city !== 'Any') {
+    query = query.where('city', '==', filters.city);
+  }
+  if(filters.price !== 'Any'){
+    query = query.where('price', '==', filters.price.length);
+  }
+
+  if(filters.sort === 'Ratings'){
+    query = query.orderBy('avgRating', 'desc');
+  }
+  else if(filters.sort === 'Reviews'){
+    query = query.orderBy('numRatings', 'desc');
+  }
+
+  this.getDocumentsInQuery(query, render);
 };
 
 FriendlyEats.prototype.addRating = function(restaurantID, rating) {
