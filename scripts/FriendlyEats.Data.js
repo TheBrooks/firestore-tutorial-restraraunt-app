7e340af4b40f88aec853d6dbec0f3428af585afc
TheBrooks <the.ryan.brooks@gmail.com>
2018-02-06 13:02:05 -0700

    read and write restaraunts to firestore

--- scripts/FriendlyEats.Data.js
+++ scripts/FriendlyEats.Data.js
@@ -16,21 +16,27 @@
 'use strict';
 
 FriendlyEats.prototype.addRestaurant = function(data) {
-  /*
-    TODO: Implement adding a document
-  */
+  const collection = firebase.firestore().collection('restaurants');
+  return collection.add(data);
 };
 
 FriendlyEats.prototype.getAllRestaurants = function(render) {
-  /*
-    TODO: Retrieve list of restaurants
-  */
+  const query = firebase.firestore()
+    .collection('restaurants')
+    .orderBy('avgRating', 'desc')
+    .limit(50);
+  this.getDocumentsInQuery(query, render);
 };
 
 FriendlyEats.prototype.getDocumentsInQuery = function(query, render) {
-  /*
-    TODO: Render all documents in the provided query
-  */
+  query.onSnapshot(snapshot => {
+    if(!snapshot.size) return render();
+    snapshot.docChanges.forEach(change => {
+      if(change.type === 'added'){
+        render(change.doc);
+      }
+    });
+  })
 };
 
 FriendlyEats.prototype.getRestaurant = function(id) {
